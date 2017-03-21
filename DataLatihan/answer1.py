647aaa8010782af32b978b5f17b39e7d885305c2
AlfianR7 <InsaneALR7@gmail.com>
2017-03-21 20:45:38 +0700

    Alfian Rahman A
    Tugas 1

--- DataLatihan/answer1.py
+++ DataLatihan/answer1.py
@@ -1,5 +1,5 @@
-data1 = "Data_1.txt"
-data2 = "Data_2.txt"
+data1 = "Data1.txt"
+data2 = "Data2.txt"
 
 def readData(data1):
 	x = []
@@ -7,3 +7,14 @@ def readData(data1):
 	    for line in data :
 		    x = line.split()
 	return x
+
+x = readData(data1)
+txt = []
+for i in x:
+	if i == 'I':
+		txt.append('*')
+	elif i == 'and' or i == 'The' or i == 'you':
+		txt.append('*'*3)
+	else: txt.append(i)
+txt = ' '.join(txt)
+print(txt)
